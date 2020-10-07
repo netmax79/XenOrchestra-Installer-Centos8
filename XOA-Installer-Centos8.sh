dcf170efc4e121b84bfdb81ba9fcf1cdf0e8fe92
Carsten Schöne <cs@linux-administrator.com>
2020-10-07 15:45:48 +0200

    fix nodejs version string in download target

--- XOA-Installer-Centos8.sh
+++ XOA-Installer-Centos8.sh
@@ -27,7 +27,7 @@ echo "Please wait......"
 echo "${red}==================================="
 sleep 1
 #yum install nodejs -y  > /dev/null 2>&1
-curl -L -s -o /tmp/node-v12.18.0-linux-x64.tar.gz https://nodejs.org/dist/latest-v12.x/node-v12.19.0-linux-x64.tar.gz
+curl -L -s -o /tmp/node-v12.19.0-linux-x64.tar.gz https://nodejs.org/dist/latest-v12.x/node-v12.19.0-linux-x64.tar.gz
 tar -C /tmp -xzf /tmp/node-v12.19.0-linux-x64.tar.gz
 CURD=`pwd`
 cd /tmp/node-v12.19.0-linux-x64
